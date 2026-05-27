59a0cddd71496035029674208e32a20567cc9080
Vin <vincentrong20050210@gmail.com>
2026-05-27 13:43:36 +0200

    fix: remove profile version label

--- src/性斗学园/性斗学园脚本/components/pages/ProfilePage.vue
+++ src/性斗学园/性斗学园脚本/components/pages/ProfilePage.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="profile-page">
-    <!-- 版本号显示 -->
-    <div class="version-label">版本：2.5.6.0</div>
-
     <button class="help-btn" @click="showHelp = true" title="玩法说明">
       <i class="fas fa-question"></i>
     </button>
@@ -668,18 +665,6 @@ onMounted(() => {
     font-size: 13px;
     line-height: 1;
   }
-}
-
-.version-label {
-  position: absolute;
-  top: 16px;
-  left: 12px;
-  z-index: 30;
-  font-size: 11px;
-  color: rgba(255, 255, 255, 0.45);
-  font-weight: 400;
-  letter-spacing: 0.2px;
-  user-select: none;
 }
 
 .help-overlay {
